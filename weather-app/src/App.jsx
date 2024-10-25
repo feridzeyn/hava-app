0af56ee6937560e71b655195a40ab98d3ac2d666
feridzeyn <feridzeynalov12@gmail.com>
2024-10-25 12:09:28 +0200

    added branches

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+<>
+<Router>
+    
+</Router>
+</>
+  )
+}
---
+++ weather-app/src/Components/UI/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export default function() {
+  return (
+    <div>
+        <h1>Azərbaycan-Bölgələrə Görə Hava Məlumatı</h1>
+<ul>
+    <Link to={'regionList'}>Bölgə Siyahısı</Link>
+</ul>
+
+    </div>
+  )
+}
