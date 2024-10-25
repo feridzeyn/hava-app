be6dfd2da2c61daeea1b4c6e366e8b72b3437561
feridzeyn <feridzeynalov12@gmail.com>
2024-10-25 16:56:36 +0200

    map updated

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AzerbaijanMap from './Components/Map'
+
+export default function App() {
+  return (
+    <div>
+      <div className="place-content-center">
+      <AzerbaijanMap/>
+      </div>
+    </div>
+  )
+}
