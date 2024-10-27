65bbb780f265d93e02cf036335fedd368e23132c
User <6zmyuj2@code.edu.az>
2024-10-27 14:06:32 +0400

    new chage

--- weather-app/src/Components/Pages/Home.jsx
+++ weather-app/src/Components/Pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Navbar from '../Navbar';
 import City from '../CitiesComponents'
 
 export default function Home() {
     return (
         <div>
-          <Navbar/>
+          
           <City/>
         </div>
     )
