fbef61715f7c78228e9d4dd818bb951b26a95734
User <6zmyuj2@code.edu.az>
2024-10-27 15:38:35 +0400

    new change

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -6,11 +6,6 @@ import Home from "./Components/Pages/Home";
 import Apexchart from './Components/ApexChart';
 
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import AzerbaijanMap from './Components/Pages/AzerbaijanMap'
-import Default from "./Components/Layouts/Default"
-import Home from "./Components/Pages/Home"
-
 
 
 export default function App() {
