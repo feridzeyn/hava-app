2ba12163ec9285c8bec0b04464b69982c006f7d6
User <6zmyuj2@code.edu.az>
2024-10-30 01:23:19 +0400

    little changes

--- weather-app/src/Components/ApexChart.jsx
+++ weather-app/src/Components/ApexChart.jsx
@@ -13,30 +13,375 @@ const WeatherPage = () => {
   const [selectedChart, setSelectedChart] = useState("temperature");
 
   const [temperatureChartData, setTemperatureChartData] = useState({
-    series: [{ name: "Temperature (°C)", data: [] }],
+   
     options: {
-      chart: { height: 350, type: "line" },
-      xaxis: { categories: [] },
-      yaxis: { title: { text: "Temperature (°C)" }, min: -10, max: 40 },
+
+      chart: {
+        height: 950,
+        type: "area",
+        animations: {
+          enabled: false,
+          easing: "easeinout",
+          speed: 800,
+        },
+        toolbar: {
+          show: false,
+        },
+        dropShadow: {
+          enabled: true,
+        },
+        zoom: {
+          enabled: false,
+        },
+      },
+      noData: {
+        text: "loading",
+      },
+      tooltip: {
+        marker: {
+          show: false,
+        },
+        fixed: {
+          enabled: false,
+          // position: "bottomLeft",
+          // offsetX: 20,
+          // offsetY: -20,
+        },
+      },
+
+      grid: {
+        show: false,
+      },
+
+      plotOptions: {
+        area: {
+          fillTo: "end",
+        },
+      },
+
+      yaxis: {
+        show: true,
+        labels: {
+          show: false,
+        },
+      },
+
+      stroke: {
+        width: 2,
+        colors: [], // CHANGE
+      },
+      series: [
+        {
+          series: [{ name: "Temperature (°C)",}], //CHANGE
+          data: [setTemperatureChartData], //CHANGE; derece ile yaz
+        },
+      ],
+
+      xaxis: {
+        categories: [],
+        axisBorder: {
+          height: 0,
+        },
+        axisTicks: {
+          height: 0,
+        },
+        labels: {
+          style: {
+            colors: [],
+          },
+        },
+      },
+
+      markers: {
+        colors: ["#fff"],
+        shape: "circle",
+        radius: 1,
+      },
+
+      dataLabels: {
+        enabled: true,
+        offsetY: -10,
+        style: {
+          fontSize: "10px",
+          fontFamily: "Arial, Helvetica, sans-serif",
+          fontWeight: 200,
+          colors: ["transparent"],
+        },
+        background: {
+          borderWidth: 0,
+        },
+      },
+
+      title: {
+        margin: 0,
+        offsetX: 10,
+        style: {
+          fontSize: "16px",
+          fontWeight: "bold",
+        },
+      },
+
+      fill: {
+        colors: [], // CHANGE
+        type: "gradient",
+        gradient: {
+          shade: "light",
+          type: "vertical",
+          shadeIntensity: 0.5,
+          opacityFrom: 0.5,
+          opacityTo: 0.1,
+          stops: [0, 100],
+        },
+
+      },
     },
   });
 
   const [humidityChartData, setHumidityChartData] = useState({
-    series: [{ name: "Humidity (%)", data: [] }],
+   
     options: {
-      chart: { height: 350, type: "line" },
-      xaxis: { categories: [] },
-      yaxis: { title: { text: "Humidity (%)" }, min: 0, max: 100 },
+      chart: {
+        height: 950,
+        type: "area",
+        animations: {
+          enabled: false,
+          easing: "easeinout",
+          speed: 800,
+        },
+        toolbar: {
+          show: false,
+        },
+        dropShadow: {
+          enabled: true,
+        },
+        zoom: {
+          enabled: false,
+        },
+      },
+      noData: {
+        text: "loading",
+      },
+      tooltip: {
+        marker: {
+          show: false,
+        },
+        fixed: {
+          enabled: false,
+          // position: "bottomLeft",
+          // offsetX: 20,
+          // offsetY: -20,
+        },
+      },
+
+      grid: {
+        show: false,
+      },
+
+      plotOptions: {
+        area: {
+          fillTo: "end",
+        },
+      },
+
+      yaxis: {
+        show: true,
+        labels: {
+          show: false,
+        },
+      },
+
+      stroke: {
+        width: 2,
+        colors: [], // CHANGE
+      },
+      series: [
+        {
+          name: ["Humidity (%)", ], //CHANGE
+          data: [setHumidityChartData], //CHANGE; derece ile yaz
+        },
+      ],
+
+      xaxis: {
+        categories: [],
+        axisBorder: {
+          height: 0,
+        },
+        axisTicks: {
+          height: 0,
+        },
+        labels: {
+          style: {
+            colors: [],
+          },
+        },
+      },
+
+      markers: {
+        colors: ["#fff"],
+        shape: "circle",
+        radius: 1,
+      },
+
+      dataLabels: {
+        enabled: true,
+        offsetY: -10,
+        style: {
+          fontSize: "10px",
+          fontFamily: "Arial, Helvetica, sans-serif",
+          fontWeight: 200,
+          colors: ["transparent"],
+        },
+        background: {
+          borderWidth: 0,
+        },
+      },
+
+      title: {
+        margin: 0,
+        offsetX: 10,
+        style: {
+          fontSize: "16px",
+          fontWeight: "bold",
+        },
+      },
+
+      fill: {
+        colors: [], // CHANGE
+        type: "gradient",
+        gradient: {
+          shade: "light",
+          type: "vertical",
+          shadeIntensity: 0.5,
+          opacityFrom: 0.5,
+          opacityTo: 0.1,
+          stops: [0, 100],
+        },
+      },
     },
   });
 
   const [weatherDescriptionChartData, setWeatherDescriptionChartData] =
     useState({
-      series: [{ name: "Weather Description", data: [] }],
+      
       options: {
-        chart: { height: 350, type: "line" },
-        xaxis: { categories: [] },
-        yaxis: { title: { text: "Weather Description" } },
+        chart: {
+          height: 950,
+          type: "area",
+          animations: {
+            enabled: false,
+            easing: "easeinout",
+            speed: 800,
+          },
+          toolbar: {
+            show: false,
+          },
+          dropShadow: {
+            enabled: true,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+        noData: {
+          text: "loading",
+        },
+        tooltip: {
+          marker: {
+            show: false,
+          },
+          fixed: {
+            enabled: false,
+            // position: "bottomLeft",
+            // offsetX: 20,
+            // offsetY: -20,
+          },
+        },
+
+        grid: {
+          show: false,
+        },
+
+        plotOptions: {
+          area: {
+            fillTo: "end",
+          },
+        },
+
+        yaxis: {
+          show: true,
+          labels: {
+            show: false,
+          },
+        },
+
+        stroke: {
+          width: 2,
+          colors: [], // CHANGE
+        },
+        series: [
+          {
+            name: "Weather Description",
+            data: setWeatherDescriptionChartData,//CHANGE
+           
+          },
+        ],
+
+        xaxis: {
+          categories: [],
+          axisBorder: {
+            height: 0,
+          },
+          axisTicks: {
+            height: 0,
+          },
+          labels: {
+            style: {
+              colors: [],
+            },
+          },
+        },
+
+        markers: {
+          colors: ["#fff"],
+          shape: "circle",
+          radius: 1,
+        },
+
+        dataLabels: {
+          enabled: true,
+          offsetY: -10,
+          style: {
+            fontSize: "10px",
+            fontFamily: "Arial, Helvetica, sans-serif",
+            fontWeight: 200,
+            colors: ["transparent"],
+          },
+          background: {
+            borderWidth: 0,
+          },
+        },
+
+        title: {
+          margin: 0,
+          offsetX: 10,
+          style: {
+            fontSize: "16px",
+            fontWeight: "bold",
+          },
+        },
+
+        fill: {
+          colors: [], // CHANGE
+          type: "gradient",
+          gradient: {
+            shade: "light",
+            type: "vertical",
+            shadeIntensity: 0.5,
+            opacityFrom: 0.5,
+            opacityTo: 0.1,
+            stops: [0, 100],
+          },
+        },
       },
     });
 
@@ -118,7 +463,7 @@ const WeatherPage = () => {
             options={humidityChartData.options}
             series={humidityChartData.series}
             type="area"
-            height={350}
+            height={450}
           />
         );
       case "weather":
@@ -127,7 +472,7 @@ const WeatherPage = () => {
             options={weatherDescriptionChartData.options}
             series={weatherDescriptionChartData.series}
             type="area"
-            height={350}
+            height={450}
           />
         );
       case "temperature":
@@ -137,7 +482,7 @@ const WeatherPage = () => {
             options={temperatureChartData.options}
             series={temperatureChartData.series}
             type="area"
-            height={350}
+            height={650}
           />
         );
     }
