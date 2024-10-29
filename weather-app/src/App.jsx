aa5e8f0a2fcb4eae5e5bff2989d9e9ed44726e77
User <6zmyuj2@code.edu.az>
2024-10-29 16:49:26 +0400

    change

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AzerbaijanMap from "./Components/Pages/AzerbaijanMap";
 import Default from "./Components/Layouts/Default";
 import Home from "./Components/Pages/Home";
-import ApexChart from "./Components/ApexChart";
+import ApexCharts from "./Components/ApexChart";
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
           <Route element={<Default />}>
             <Route path="/" element={<Home />} />
             <Route path="/azerbaijanMap" element={<AzerbaijanMap />} />
-            <Route path="/apexchart/:cityName" element={<ApexChart />} />
+            <Route path="/apexchart" element={<ApexCharts />} />
           </Route>
         </Routes>
       </Router>
