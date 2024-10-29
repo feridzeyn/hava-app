9766d272ec7571073666af9d4d927a4f07ba60d1
User <6zmyuj2@code.edu.az>
2024-10-29 16:38:30 +0400

    check dev

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,29 +1,21 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AzerbaijanMap from "./Components/Pages/AzerbaijanMap";
 import Default from "./Components/Layouts/Default";
 import Home from "./Components/Pages/Home";
-import Apexchart from './Components/ApexChart';
-
-
-
+import ApexChart from "./Components/ApexChart";
 
 export default function App() {
   return (
     <div>
-
-  <Router>
-    <Routes>
-      <Route element={<Default/>}>
-      <Route path="/" element={<Home/>} />
-      <Route path="/azerbaijanMap" element={<AzerbaijanMap/>}/>
-
-      </Route>
-    </Routes>
-  </Router>
-
-     
+      <Router>
+        <Routes>
+          <Route element={<Default />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/azerbaijanMap" element={<AzerbaijanMap />} />
+            <Route path="/apexchart/:cityName" element={<ApexChart />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
-  )
-
+  );
 }
